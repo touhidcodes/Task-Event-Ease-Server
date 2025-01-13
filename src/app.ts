7721f25a43dcba59b36ec53eda233d76e0b2070b
touhidcodes <touhidcodes@gmail.com>
2025-01-13 10:45:19 +0600

    upd: app file

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,46 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import httpStatus from "http-status";
+import router from "./app/router/routes";
+import cookieParser from "cookie-parser";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 dotenv.config();
 
 const app: Application = express();
 
+const corsOptions = {
+  origin: ["https://flat-mate-finder.vercel.app", "http://localhost:3000"],
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+};
+
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 // Application Routes
-// Import and use your routes here
+app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World !.. Server is running...");
+  res.send("Server is running... !");
+});
+
+app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
 });
 
 export default app;
